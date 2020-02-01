c15b2ef81272921b432fa237cf0d689dbc1524b8
Mark G <mark.garcia.8001@gmail.com>
2020-01-31 17:22:13 -0800

    Added JavaDoc

--- wargame/src/Card.java
+++ wargame/src/Card.java
@@ -4,16 +4,29 @@ public class Card {
     private int ranking; // 2-10, Jack (11), Queen (12), King (13), Ace (either 1 or 14 depending on the game being played.)
     private int suit; // 0-3 Clubs Spades Hearts Diamonds
 
-
+    /**
+     * Creates a Card with a suit(0-3) and a rank (2-14)
+     * @param s int of suit
+     * @param r int of rank
+     */
     public Card(int s, int r){
         this.suit = s;
         this.ranking = r;
     }
 
+    /**
+     * Gets the rank/number of the card
+     * @return cards ranking
+     */
     public int getRank(){
         return ranking;
     }
 
+    /**
+     * Gets the suit of the card. Since suit is taken as an int there needs to be the if/else statements to
+     * return the suit as a word
+     * @return String of the suit of the card
+     */
     public String getSuit(){
         String suitName = "";
         if(suit == 0){suitName = "Clubs";}
@@ -23,6 +36,10 @@ public class Card {
         return suitName;
     }
 
+    /**
+     * Creates a string of the card.
+     * @return a String in the form "Rank of Suit"
+     */
     @Override
     public String toString(){
         StringBuffer card = new StringBuffer();
--- wargame/src/Deck.java
+++ wargame/src/Deck.java
@@ -4,31 +4,67 @@ import java.util.ArrayList;
 public class Deck {
   private ArrayList<Card> deck;
 
+  /**
+   * Constructor of a deck object which as ArrayList of Card objects.
+   */
   public Deck() {
     deck = new ArrayList<>();
   }
+
+  /**
+   * Adds a card to the deck
+   * @param c card to be added to the end of the deck
+   */
   public void addCard(Card c){
     deck.add(c);
   }
+
+  /**
+   * Remove card from deck at index i
+   * @param i index of card to be removed.
+   */
   public void removeCard(int i){
     deck.remove(i);
   }
+
+  /**
+   * Gets the card at index i
+   * @param i index of card in array list
+   * @return the card at index i
+   * @throws IndexOutOfBoundsException
+   */
   public Card getCard(int i) throws IndexOutOfBoundsException{
     return deck.get(i);
   }
 
+  /**
+   * Takes 4 parameters, 2 are cards and 2 are those cards index.
+   * swaps the two cards at their appropriate index.
+   * @param c1 card to be moved
+   * @param c2 card to be moved
+   * @param num1 position to move card 2
+   * @param num2 position to move card 1
+   */
   public void swapCards(Card c1, Card c2, int num1, int num2){
     deck.set(num1, c2);
     deck.set(num2, c1);
   }
 
+  /**
+   * Gets the size of the deck.
+   * @return deck size as an integer
+   */
   public int getDeckSize(){
     return deck.size();
   }
 
+  /**
+   * Overrides the default toString() method and creates a string with all the cards in the current deck.
+   * @return String of all cards the deck has
+   */
   @Override
   public String toString(){
-    String s = "This player has: ";
+    String s = "This deck has: ";
     for(Card c : deck){
       s += c.toString() + ",";
     }//end for loop
